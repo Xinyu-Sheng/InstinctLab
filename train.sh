e95fbeba400c7c0092ebc2fb6b042d1e19db973a
Xinyu Sheng <sheng.xin.yu@foxmail.com>
2026-03-23 00:59:48 +0800

    [fix] 更新训练脚本，调整num_envs参数为1024

--- train.sh
+++ train.sh
@@ -20,6 +20,6 @@ srun python -m torch.distributed.run \
  scripts/instinct_rl/train.py \
   --task=Instinct-Parkour-Target-Amp-G1-v0 \
   --distributed \
-  --num_envs=128 \
+  --num_envs=1024 \
   --headless
   
