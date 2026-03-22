ed3409e2f83209294a677f3400958164890ea234
Xinyu Sheng <sheng.xin.yu@foxmail.com>
2026-03-22 20:29:04 +0800

    [fix] 更新训练脚本，修正作业名称、任务数和环境变量设置

--- sbatch/train.sh
+++ sbatch/train.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=sheng_train
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=16
-#SBATCH --gres=gpu:4  # 必须保留：申请GPU资源
-#SBATCH --output=train_ddp_%j.out
-#SBATCH --error=train_ddp_%j.err
-#SBATCH --nodelist=3090node1
+#SBATCH --job-name=train_xy      
+#SBATCH --nodes=1                    
+#SBATCH --ntasks-per-node=1          
+#SBATCH --cpus-per-task=96            
+#SBATCH --output=sbatch/train_%j.out     
+#SBATCH --error=sbatch/train_%j.err      
+#SBATCH --nodelist=3090node1  
 
 # 环境变量（仅保留NCCL相关，去掉MASTER_PORT/ADDR）
 export NCCL_SOCKET_IFNAME=eno2
 export NCCL_DEBUG=INFO
-export OMP_NUM_THREADS=1
-export MASTER_PORT=$((29500 + RANDOM % 20000))
+
 # 激活环境
 source /mnt/slurmfs-4090node1/homes/xsheng420/miniconda3/bin/activate instinct
 
@@ -22,5 +20,5 @@ srun python -m torch.distributed.run \
   ../scripts/instinct_rl/train.py \
   --task=Instinct-Parkour-Target-Amp-G1-v0 \
   --distributed \
-  --num_envs=512
+  --num_envs=256
   
