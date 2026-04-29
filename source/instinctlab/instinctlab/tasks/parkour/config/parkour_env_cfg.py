b459e2f8c8a150b7f6fc634ca2c017e559080c81
Xinyu Sheng <sheng.xin.yu@foxmail.com>
2026-04-30 00:21:40 +0800

    [fix] 调整G1奖励中的静止惩罚权重以增强移动行为的惩罚力度

--- source/instinctlab/instinctlab/tasks/parkour/config/parkour_env_cfg.py
+++ source/instinctlab/instinctlab/tasks/parkour/config/parkour_env_cfg.py
@@ -764,7 +764,7 @@ class G1Rewards:
     # 当没有速度指令时，对移动行为进行惩罚。
     stand_still = RewTerm(
         func=mdp.stand_still,
-        weight=-0.3,
+        weight=-3.0,
         params={"command_name": "base_velocity", "offset": 4.0},
     )
 
